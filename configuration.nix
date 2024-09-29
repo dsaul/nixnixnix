fdddecfaa60276fb8788062d44dc1af61e08c10e
Dan <dan@dsaul.ca>
2024-09-29 16:14:23 -0500

    .

--- configuration.nix
+++ configuration.nix
@@ -124,6 +124,19 @@
   onlyoffice-bin
   ];
   
+  fonts.packages = with pkgs; [
+  noto-fonts
+  noto-fonts-cjk
+  noto-fonts-emoji
+  liberation_ttf
+  fira-code
+  fira-code-symbols
+  mplus-outline-fonts.githubRelease
+  dina-font
+  proggyfonts
+  nerdfonts
+];
+  
   programs.appimage.enable = true;
   programs.appimage.binfmt = true;
 
