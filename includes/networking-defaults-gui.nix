4c0b76e285f384bf871dedba68550807c720e022
Dan Saul <dan@dsaul.ca>
2024-12-07 10:25:38 -0600

    .

--- includes/networking-defaults-gui.nix
+++ includes/networking-defaults-gui.nix
@@ -121,26 +121,26 @@ in
 			"media.video_stats.enabled" = false;
 
 			# Privacy Settings
-			"network.http.referer.spoofSource" = true;
-			"network.dns.disablePrefetchFromHTTPS" = true;
-			"network.dns.disablePrefetch" = true;
-			"network.http.speculative-parallel-limit" = 0;
-			"network.predictor.enable-prefetch" = false;
-			"network.predictor.enabled" = false;
-			"network.prefetch-next" = false;
-			"network.trr.mode" = 5;
-			"privacy.donottrackheader.enabled" = true;
-			"privacy.donottrackheader.value" = 1;
-			"privacy.query_stripping" = true;
-			"privacy.trackingprotection.cryptomining.enabled" = true;
-			"privacy.trackingprotection.enabled" = true;
-			"privacy.trackingprotection.fingerprinting.enabled" = true;
-			"privacy.trackingprotection.pbmode.enabled" = true;
-			"privacy.usercontext.about_newtab_segregation.enabled" = true;
-			"security.ssl.disable_session_identifiers" = true;
-			"signon.autofillForms" = false;
-			"webgl.renderer-string-override" = " ";
-			"webgl.vendor-string-override" = " ";
+			#"network.http.referer.spoofSource" = true;
+			#"network.dns.disablePrefetchFromHTTPS" = true;
+			#"network.dns.disablePrefetch" = true;
+			#"network.http.speculative-parallel-limit" = 0;
+			#"network.predictor.enable-prefetch" = false;
+			#"network.predictor.enabled" = false;
+			#"network.prefetch-next" = false;
+			#"network.trr.mode" = 5;
+			#"privacy.donottrackheader.enabled" = true;
+			#"privacy.donottrackheader.value" = 1;
+			#"privacy.query_stripping" = true;
+			#"privacy.trackingprotection.cryptomining.enabled" = true;
+			#"privacy.trackingprotection.enabled" = true;
+			#"privacy.trackingprotection.fingerprinting.enabled" = true;
+			#"privacy.trackingprotection.pbmode.enabled" = true;
+			#"privacy.usercontext.about_newtab_segregation.enabled" = true;
+			#"security.ssl.disable_session_identifiers" = true;
+			#"signon.autofillForms" = false;
+			#"webgl.renderer-string-override" = " ";
+			#"webgl.vendor-string-override" = " ";
 
 			# Extensions
 			"extensions.FirefoxMulti-AccountContainers@mozilla.whiteList" = "";
