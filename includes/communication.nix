2ce8cdf29e7873b0eda0601da3c4e8132c127e5e
Dan Saul <dan@dsaul.ca>
2024-12-13 13:08:03 -0600

    .

--- includes/communication.nix
+++ includes/communication.nix
@@ -5,7 +5,10 @@
 	#  [ (modulesPath + "/installer/scan/not-detected.nix")
 	#  ];
 	
-	
+	config.permittedInsecurePackages = [
+		"olm-3.2.16"
+	];
+
 	environment.systemPackages = with pkgs; [
 		#discord
 		#(discord.override {
