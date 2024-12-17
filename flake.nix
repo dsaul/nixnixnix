7a2c1de7f2e2d338a2f1cc75fb07f5dd81736bc9
Dan <dan@dsaul.ca>
2024-12-17 15:46:48 -0600

    .

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
         nixpkgs.url        = "github:NixOS/nixpkgs/nixos-24.11";
         nixos-hardware.url = "github:NixOS/nixos-hardware";
         home-manager.url = "github:nix-community/home-manager/release-24.11";
-        unstable.url = "https://nixos.org/channels/nixos-unstable";
+        unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     };
 
     outputs = { self, nixpkgs, nix, nixos-hardware, home-manager}: {
