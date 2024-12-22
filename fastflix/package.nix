da05e3517f78a597902936410f8199dc78690c9b
Dan Saul <dan@dsaul.ca>
2024-12-22 12:02:00 -0600

    .

--- fastflix/package.nix
+++ fastflix/package.nix
@@ -10,7 +10,7 @@ stdenv.mkDerivation (finalAttrs: {
 
 	src = fetchurl {
 		url = "https://github.com/cdgriffith/FastFlix/archive/refs/tags/${finalAttrs.version}.tar.gz";
-		hash = "sha256-543949b0426d751858029f29384008a79968bfe5147117f6f490834fcdad4645";
+		hash = "sha256-3af7312cf5f6238a9bd6fb4f23b7907442219818590a82086010cb8c22b1c25c";
 	};
 
 
