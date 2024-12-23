ae2e6bfb6093e189af9d64c5a704e24d03b66ca8
Dan Saul <dan@dsaul.ca>
2024-12-22 22:10:24 -0600

    .

--- NVEnc/package.nix
+++ NVEnc/package.nix
@@ -15,18 +15,31 @@ stdenv.mkDerivation rec {
 	version = "7.77";
 	
 	src = pkgs.fetchurl
-    {
-      url = "https://github.com/rigaya/NVEnc/releases/download/${version}/nvencc_${version}_Ubuntu24.04_amd64.deb";
-      hash = "sha256-yl9VSFnKZrhwDGfAjcKbFclghXy2xJtWceAcESyZw9E=";
-    };
+	{
+		url = "https://github.com/rigaya/NVEnc/releases/download/${version}/nvencc_${version}_Ubuntu24.04_amd64.deb";
+		hash = "sha256-yl9VSFnKZrhwDGfAjcKbFclghXy2xJtWceAcESyZw9E=";
+	};
 	
 	unpackPhase = "dpkg-deb -X $src $out";
 	
-	nativeBuildInputs = with pkgs; [ dpkg autoPatchelfHook ];
+	nativeBuildInputs = with pkgs; [
+		dpkg
+		autoPatchelfHook
+	];
+
+	buildInputs = with pkgs; [
+		glibc
+		gcc
+		libass
+		ffmpeg
+		cudatoolkit
+	];
+	# https://nixos.org/manual/nixpkgs/stable/#setup-hook-autopatchelfhook
+	#autoPatchelfIgnoreMissingDeps = [ "libcuda.so.1" "libcudart.so.1" ]; or to [ "*" ] to ignore all missing dependencies.
 	
 	installPhase = ''
-      ls -l $src
-    '';
+		ls -l $out
+	'';
 	
 	
 }
