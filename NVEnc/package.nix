979caa03a4be5d54234455092804893b875a88ba
Dan Saul <dan@dsaul.ca>
2024-12-22 22:00:22 -0600

    .

--- NVEnc/package.nix
+++ NVEnc/package.nix
@@ -13,12 +13,11 @@
 stdenv.mkDerivation rec {
 	pname = "NVEnc";
 	version = "7.77";
-	format = "pyproject";
 	
 	src = pkgs.fetchurl
     {
       url = "https://github.com/rigaya/NVEnc/releases/download/${version}/nvencc_${version}_Ubuntu24.04_amd64.deb";
-      hash = "sha256-aWAvwtGrFH2HUNkzLW1WqukIy4NemN9Vrb/DMGYGf5Y=";
+      hash = "sha256-yl9VSFnKZrhwDGfAjcKbFclghXy2xJtWceAcESyZw9E=";
     };
 	
 	unpackPhase = "dpkg-deb -x $src unpack";
