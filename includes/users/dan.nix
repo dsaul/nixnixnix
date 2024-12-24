bf95585c35be041d1d57f049d740b41758d17671
Dan Saul <dan@dsaul.ca>
2024-12-24 17:26:24 -0600

    .

--- includes/users/dan.nix
+++ includes/users/dan.nix
@@ -1,25 +1,12 @@
 { config, lib, pkgs, modulesPath, ... }:
-let
-	icon = (pkgs.stdenv.mkDerivation rec {
-			pname = "dan-icon";
-			version = "1";
-			
-			src = ./dan.png;
-			
-			dontUnpack = true;
-			
-			installPhase = ''
-				mkdir -p $out/etc/usericons.d/
-				cp ${src} $out/etc/usericons.d/dan
-			'';
-			
-			# /var/lib/AccountsService/icons/
-			
-		});
-in
 {
+	environment.etc = {
+		"usericons.d/dan".source = ${./dan.png};
+	}
+	
+	
 	
 	environment.systemPackages = with pkgs; [
-		icon
+		
 	];	
 }
