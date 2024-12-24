c3152b5143fe878a823bfed7b285a2fdf23f1581
Dan Saul <dan@dsaul.ca>
2024-12-24 16:44:10 -0600

    .

--- includes/users/dan.nix
+++ includes/users/dan.nix
@@ -2,17 +2,17 @@
 
 {
 	
-	environment.systemPackages = with pkgs; [
-		(stdenv.mkDerivation rec {
-			pname = "dan-icon";
-			version = "1";
-			
-			installPhase = ''
-				cp ${./dan.png} $out/var/lib/AccountsService/icons/dan
-			'';
-			
-			# /var/lib/AccountsService/icons/
-			
-		})
-	];	
+	#environment.systemPackages = with pkgs; [
+	#	(stdenv.mkDerivation rec {
+	#		pname = "dan-icon";
+	#		version = "1";
+	#		
+	#		installPhase = ''
+	#			cp ${./dan.png} $out/var/lib/AccountsService/icons/dan
+	#		'';
+	#		
+	#		# /var/lib/AccountsService/icons/
+	#		
+	#	})
+	#];	
 }
