7ef92b9688ee75004ee76a7fc0231eb88de747b3
Dan Saul <dan@dsaul.ca>
2024-12-30 16:41:57 -0600

    .

--- includes/services-reverse-proxy.nix
+++ includes/services-reverse-proxy.nix
@@ -29,12 +29,18 @@
 		recommendedOptimisation = true;
 		
 		virtualHosts."calendar.dsaul.ca" = {
-			#forceSSL = true;
+			forceSSL = true;
 			useACMEHost = "dsaul.ca";
 			
 			locations."/" = {
 				proxyPass = "http://10.5.5.10:9900";
 				proxyWebsockets = true; # needed if you need to use WebSocket
+				
+				extraConfig = ''
+					proxy_ssl_server_name on;
+					rewrite ^/.well-known/carddav /dav/ redirect;
+					rewrite ^/.well-known/caldav /dav/ redirect;
+				'';
 			};
 			
 			
