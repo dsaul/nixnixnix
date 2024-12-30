04a47de26113a91c254ba909efb5dd33dc0bcb10
Dan Saul <dan@dsaul.ca>
2024-12-30 12:26:05 -0600

    .

--- users/root/default.nix
+++ users/root/default.nix
@@ -9,7 +9,6 @@ in
 	users.users."${username}" = {
 		hashedPasswordFile = config.age.secrets."userHashedPasswordFile-${username}".path;
 		openssh.authorizedKeys.keys = [
-			"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO4DXCWnspO5WUrirR33EAGTIl692+COgeds0Tvtw6Yd"
-		];
+		] ++ users.users.dan.openssh.authorizedKeys.keys;
 	};
 }
