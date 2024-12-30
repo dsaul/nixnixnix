957d6afad69f7ad5b7cf13692cf600a92cf2bae6
Dan Saul <dan@dsaul.ca>
2024-12-30 15:11:35 -0600

    .

--- cornwall-fileserver/configuration.nix
+++ cornwall-fileserver/configuration.nix
@@ -36,7 +36,7 @@ in
 		
 	];	
 	
-	networking.networkmanager.insertNameservers = [ "1.1.1.1" "9.9.9.9" ];
+	#networking.networkmanager.insertNameservers = [ "1.1.1.1" "9.9.9.9" ];
 	
 
 	# This value determines the NixOS release from which the default
