48a2f9465dd9af8a1dcc8e9a777cbcfb77092dfc
Dan Saul <dan@dsaul.ca>
2024-12-31 15:05:15 -0600

    .

--- services/http-vhost/http-vhost-mealie.dsaul.ca.nix
+++ services/http-vhost/http-vhost-mealie.dsaul.ca.nix
@@ -13,7 +13,7 @@
 			useACMEHost = "dsaul.ca";
 			
 			locations."/" = {
-				proxyPass = "http://10.5.5.10:9091";
+				proxyPass = "http://10.5.5.5:9091";
 				proxyWebsockets = true; # needed if you need to use WebSocket
 				
 				extraConfig = ''
