8317996d6d0008f209a22037768051701de0131e
Dan Saul <dan@dsaul.ca>
2024-12-31 17:13:36 -0600

    .

--- stacks/jellyfin/jellyfin.nix
+++ stacks/jellyfin/jellyfin.nix
@@ -18,9 +18,11 @@ services:
     user: "${UID}:${GID}"
     image: jellyfin/jellyfin
     container_name: ${packageName}
-    network_mode: "host"
     ports:
-      - "8096:8096"
+      - 8096:8096
+	  - 8920:8920
+	  - 1900:1900/udp
+	  - 7359:7359/udp
     volumes:
       - /var/stacks/${packageName}/data-config:/config
       - /var/stacks/${packageName}/data-cache:/cache
