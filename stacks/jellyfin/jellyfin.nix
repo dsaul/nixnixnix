85f2162386904b9eca8813689fdafdf8e5c82ef4
Dan Saul <dan@dsaul.ca>
2024-12-31 16:58:36 -0600

    .

---
+++ stacks/jellyfin/jellyfin.nix
@@ -0,0 +1,71 @@
+{ config, lib, pkgs, modulesPath, ... }:
+let
+	packageName = "jellyfin";
+	UID = "0";
+	GID = "0";
+in
+{
+	#imports = [
+	#	../../services/http-vhost/http-vhost-mealie.dsaul.ca.nix
+	#];
+	
+	
+	config.environment.etc."stacks/${packageName}/compose.yaml".text =
+      /* yaml */
+      ''
+services:
+  ${packageName}:
+    user: "${UID}:${GID}"
+    image: jellyfin/jellyfin
+    container_name: ${packageName}
+    network_mode: "host"
+    ports:
+      - "8096:8096"
+    volumes:
+      - /var/stacks/${packageName}/data-config:/config
+      - /var/stacks/${packageName}/data-cache:/cache
+      - /srv/MEDIA-01:/srv/MEDIA-01:ro
+      - /srv/MEDIA-02:/srv/MEDIA-02:ro
+      - /srv/MEDIA-03:/srv/MEDIA-03:ro
+      - /srv/MEDIA-04:/srv/MEDIA-04:ro
+      - /srv/MEDIA-05:/srv/MEDIA-05:ro
+      - /srv/MEDIA-06:/srv/MEDIA-06:ro
+      - /srv/MEDIA-07:/srv/MEDIA-07:ro
+      - /srv/MEDIA-08:/srv/MEDIA-08:ro
+      - /srv/MEDIA-09:/srv/MEDIA-09:ro
+      - /srv/MEDIA-10:/srv/MEDIA-10:ro
+      - /srv/MEDIA-11:/srv/MEDIA-11:ro
+      - /srv/MEDIA-12:/srv/MEDIA-12:ro
+      - /srv/MEDIA-13:/srv/MEDIA-13:ro
+      - /srv/MEDIA-14:/srv/MEDIA-14:ro
+      - /srv/MEDIA-15:/srv/MEDIA-15:ro
+      - /srv/MEDIA-16:/srv/MEDIA-16:ro
+      - /srv/MEDIA-17:/srv/MEDIA-17:ro
+      - /srv/MEDIA-18:/srv/MEDIA-18:ro
+      - /srv/MISC-01/Audio/Music:/srv/MISC-01/Audio/Music:ro
+    restart: "unless-stopped"
+    environment:
+	  PUID: ${UID}
+      PGID: ${GID}
+'';
+	
+	config.systemd.services."${packageName}" = {
+		wantedBy = ["multi-user.target"];
+		after = ["docker.service" "docker.socket"];
+		path = [pkgs.docker];
+		script = ''
+			docker compose -f /etc/stacks/${packageName}/compose.yaml up --remove-orphans
+		'';
+		restartTriggers = [
+			config.environment.etc."stacks/${packageName}/compose.yaml".source
+		];
+	};
+	
+	config.system.activationScripts.makeWhishperDirs = lib.stringAfter [ "var" ] ''
+		mkdir -p /var/stacks/${packageName}/data
+		chown -R ${UID}:${GID} /var/stacks/${packageName}/data
+	'';
+	
+	config.networking.firewall.allowedTCPPorts = [ 8096 ];
+	#config.networking.firewall.allowedUDPPorts = [ 8096 ];
+}
---
+++ stacks/navidrome/navidrome.nix
@@ -0,0 +1,71 @@
+{ config, lib, pkgs, modulesPath, ... }:
+let
+	packageName = "navidrome";
+	UID = "0";
+	GID = "0";
+in
+{
+	imports = [
+		../../services/http-vhost/http-vhost-mealie.dsaul.ca.nix
+	];
+	
+	
+	config.environment.etc."stacks/${packageName}/compose.yaml".text =
+      /* yaml */
+      ''
+version: "3"
+services:
+  ${packageName}:
+    user: "${UID}:${GID}"
+    image: deluan/navidrome:latest
+    container_name: ${packageName}
+    ports:
+      - "4533:4533"
+    environment:
+      PUID: ${UID}
+      PGID: ${GID}
+      TZ: 'America/Winnipeg'
+      ND_SCANSCHEDULE: 1h
+      ND_LOGLEVEL: info
+      ND_BASEURL: ""
+      ND_LASTFM_ENABLED: true
+      ND_LASTFM_APIKEY: cc84840956aa5bfec224b69e9628b57c
+      ND_LASTFM_SECRET: d0413083144a3b1aace0f8672d742482
+      ND_LASTFM_LANGUAGE: en
+      ND_SPOTIFY_ID: 4fb134daf4ac4b439ae556f30cb591e8
+      ND_SPOTIFY_SECRET: 85178ea193ad42aead8d551e3bdc630c
+      ND_REVERSEPROXYUSERHEADER: Remote-User
+      ND_REVERSEPROXYWHITELIST: 10.0.0.0/8
+    volumes:
+      - "/var/stacks/${packageName}/data-navidrome:/data"
+      - "/srv/MISC-01/Audio/Music:/music:ro"
+    networks:
+      - backend
+    restart: unless-stopped
+networks:
+  backend:
+    name: backend
+    external: true
+
+'';
+	
+	config.systemd.services."${packageName}" = {
+		wantedBy = ["multi-user.target"];
+		after = ["docker.service" "docker.socket"];
+		path = [pkgs.docker];
+		script = ''
+			docker compose -f /etc/stacks/${packageName}/compose.yaml up --remove-orphans
+		'';
+		restartTriggers = [
+			config.environment.etc."stacks/${packageName}/compose.yaml".source
+		];
+	};
+	
+	config.system.activationScripts.makeWhishperDirs = lib.stringAfter [ "var" ] ''
+		mkdir -p /var/stacks/${packageName}/data
+		chown -R ${UID}:${GID} /var/stacks/${packageName}/data
+	'';
+	
+	config.networking.firewall.allowedTCPPorts = [ 4533 ];
+	#config.networking.firewall.allowedUDPPorts = [ 4533 ];
+}
