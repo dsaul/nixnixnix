e8b2fd22acff3d20ae7b43549d1dada892ae2172
Dan Saul <dan@dsaul.ca>
2025-01-02 14:48:21 -0600

    .

--- cornwall-fileserver/configuration.nix
+++ cornwall-fileserver/configuration.nix
@@ -195,30 +195,40 @@ in
 	};
 	
 	#showmount -e 10.5.5.5
-	services.nfs.server.enable = true;
-	services.nfs.server.exports = ''
-	/mnt			10.5.5.0/24(rw,nohide,sync,no_subtree_check)
-	/mnt/MEDIA-01	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
-	/mnt/MEDIA-02	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
-	/mnt/MEDIA-03	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
-	/mnt/MEDIA-04	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
-	/mnt/MEDIA-05	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
-	/mnt/MEDIA-06	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
-	/mnt/MEDIA-07	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
-	/mnt/MEDIA-08	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
-	/mnt/MEDIA-09	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
-	/mnt/MEDIA-10	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
-	/mnt/MEDIA-11	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
-	/mnt/MEDIA-12	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
-	/mnt/MEDIA-13	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
-	/mnt/MEDIA-14	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
-	/mnt/MEDIA-15	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
-	/mnt/MEDIA-16	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
-	/mnt/MEDIA-17	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
-	/mnt/MEDIA-18	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
-	'';
-
-	networking.firewall.allowedTCPPorts = [ 2049 ];
+	
+	services.nfs.server = {
+		enable = true;
+		# fixed rpc.statd port; for firewall
+		lockdPort = 4001;
+		mountdPort = 4002;
+		statdPort = 4000;
+		extraNfsdConfig = '''';
+		
+		exports = ''
+		/mnt			10.5.5.0/24(rw,nohide,sync,no_subtree_check)
+		/mnt/MEDIA-01	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
+		/mnt/MEDIA-02	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
+		/mnt/MEDIA-03	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
+		/mnt/MEDIA-04	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
+		/mnt/MEDIA-05	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
+		/mnt/MEDIA-06	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
+		/mnt/MEDIA-07	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
+		/mnt/MEDIA-08	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
+		/mnt/MEDIA-09	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
+		/mnt/MEDIA-10	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
+		/mnt/MEDIA-11	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
+		/mnt/MEDIA-12	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
+		/mnt/MEDIA-13	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
+		/mnt/MEDIA-14	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
+		/mnt/MEDIA-15	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
+		/mnt/MEDIA-16	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
+		/mnt/MEDIA-17	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
+		/mnt/MEDIA-18	10.5.5.0/24(rw,nohide,sync,no_subtree_check)
+		'';
+	};
+	
+	networking.firewall.allowedTCPPorts = [ 111 2049 4000 4001 4002 20048 ];
+	networking.firewall.allowedUDPPorts = [ 111 2049 4000 4001 4002 20048 ];
 	
 
 	# This value determines the NixOS release from which the default
