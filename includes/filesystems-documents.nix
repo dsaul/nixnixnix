21a18719680274c9d0d57156550094b453cb812a
Dan Saul <dan@dsaul.ca>
2025-01-02 15:18:42 -0600

    .

--- includes/filesystems-documents.nix
+++ includes/filesystems-documents.nix
@@ -2,13 +2,13 @@
 
 {
 	fileSystems."/mnt/MISC-01" = {
-		device = "10.5.5.5:/mnt/MISC-18";
+		device = "10.5.5.5:/mnt/MISC-01";
 		fsType = "nfs";
 		options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" "x-systemd.device-timeout=5s" "x-systemd.mount-timeout=5s" "nofail" "soft" ];
 	};
 	
 	fileSystems."/mnt/DOCUMENTS-01" = {
-		device = "10.5.5.5:/mnt/DOCUMENTS-18";
+		device = "10.5.5.5:/mnt/DOCUMENTS-01";
 		fsType = "nfs";
 		options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" "x-systemd.device-timeout=5s" "x-systemd.mount-timeout=5s" "nofail" "soft" ];
 	};
