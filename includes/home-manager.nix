6ee5b1fe3b9f9875924e50dd732d74b9835a2ae6
Dan Saul <dan@dsaul.ca>
2025-01-02 08:56:37 -0600

    .

--- includes/home-manager.nix
+++ includes/home-manager.nix
@@ -1,13 +1,13 @@
 # nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz home-manager
 # nix-channel --update
 #systemctl status "home-manager-$USER.service"
-{ config, lib, pkgs, modulesPath, home-manager, ... }:
+{ config, lib, pkgs, modulesPath, ... }:
 
 {
 	includes = [
 		<home-manager/nixos> 
 	];
-	home-manager.useGlobalPkgs = true;
+	config.home-manager.useGlobalPkgs = true;
 }
 	
 
