613c0789accfc10a9f46598c7e2646c5df52c85b
Dan Saul <dan@dsaul.ca>
2025-01-02 19:40:45 -0600

    .

--- services/http-vhost/http-vhost-calendar.dsaul.ca.nix
+++ services/http-vhost/http-vhost-calendar.dsaul.ca.nix
@@ -13,7 +13,7 @@
 			useACMEHost = "dsaul.ca";
 			
 			locations."/" = {
-				proxyPass = "http://10.5.5.10:9900";
+				proxyPass = "http://10.5.5.5:9900";
 				proxyWebsockets = true; # needed if you need to use WebSocket
 				
 				extraConfig = ''
