7d008c26571e7fb55d52ef65171f7f4014d3fd66
Dan Saul <dan@dsaul.ca>
2025-01-03 16:05:38 -0600

    .

--- services/services-reverse-proxy.nix
+++ services/services-reverse-proxy.nix
@@ -24,6 +24,22 @@
 				'';
 			};
 		};
+		
+		virtualHosts."_" = {
+			listen = [ { addr = "0.0.0.0"; port = 443; } ];
+			default = true;
+			
+			forceSSL = true;
+			useACMEHost = "dsaul.ca";
+			
+			locations."/" = {
+				
+				return = "200 '<html><body></body></html>'";
+				extraConfig = ''
+					default_type text/html;
+				'';
+			};
+		};
 	};
 	
 	networking.firewall.allowedTCPPorts = [ 80 443 ];
