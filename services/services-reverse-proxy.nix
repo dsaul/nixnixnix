6140148565ac25d6676bf953d36acb6f31966f88
Dan Saul <dan@dsaul.ca>
2025-01-03 16:02:25 -0600

    .

--- services/services-reverse-proxy.nix
+++ services/services-reverse-proxy.nix
@@ -16,7 +16,7 @@
 		virtualHosts."_" = {
 			default = true;
 			locations."/" = {
-				
+				listen = [ { addr = "0.0.0.0"; port = 80; } ];
 				return = "200 '<html><body></body></html>'";
 				extraConfig = ''
 					default_type text/html;
