3dead65df67c77b444cf2d11330da282b581a4f9
Dan Saul <dan@dsaul.ca>
2025-01-02 22:37:24 -0600

    .

--- services/services-seafile.nix
+++ services/services-seafile.nix
@@ -14,7 +14,12 @@
 		initialAdminPassword = "change this later!";
 
 		ccnetSettings.General.SERVICE_URL = "https://seafile.dsaul.ca";
-
+		# Enable weekly collection of freed blocks
+		gc = {
+			enable = true;
+			dates = [ "Sun 03:00:00" ];
+		};
+		
 		seafileSettings = {
 			history.keep_days = "14"; # Remove deleted files after 14 days
 			
@@ -23,11 +28,7 @@
 				web_token_expire_time = 18000; # Expire the token in 5h to allow longer uploads
 			};
 			
-			# Enable weekly collection of freed blocks
-			gc = {
-				enable = true;
-				dates = [ "Sun 03:00:00" ];
-			};
+			
 		};
 	};
 	
