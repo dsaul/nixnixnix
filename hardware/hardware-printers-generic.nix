de9ad3454ee0779db60518c2262f939c3bade8ab
Dan Saul <dan@dsaul.ca>
2025-01-03 13:44:00 -0600

    .

--- hardware/hardware-printers-generic.nix
+++ hardware/hardware-printers-generic.nix
@@ -3,7 +3,7 @@
 {
 	imports = [
 		../services/services-mdns.nix
-	]
+	];
 	
 	#journalctl --follow --unit=cups
 	#journalctl --follow --unit=cups | grep -C10 --color=always -i -e 'No such file or directory' -e 'error:'
