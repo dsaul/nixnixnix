afa5053955dcd093fa52500c38997ca109ef5fe8
Dan Saul <dan@dsaul.ca>
2025-01-04 10:42:00 -0600

    .

--- cornwall-fileserver/configuration.nix
+++ cornwall-fileserver/configuration.nix
@@ -43,6 +43,7 @@ in
 		../services/services-unifi.nix
 		../services/services-reverse-proxy.nix
 		../services/services-seafile.nix
+		#../../services/http-vhost/http-vhost-whishper.dsaul.ca.nix
 		
 		# Stacks
 		../stacks/mumble-server/mumble-server.nix
