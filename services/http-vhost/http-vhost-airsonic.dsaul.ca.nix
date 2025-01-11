3f5fd001b3e66edbb4c41f5a6aad4d27be020da9
Dan Saul <dan@dsaul.ca>
2025-01-11 15:20:40 -0600

    .

--- services/http-vhost/http-vhost-airsonic.dsaul.ca.nix
+++ services/http-vhost/http-vhost-airsonic.dsaul.ca.nix
@@ -18,11 +18,6 @@
 				
 				extraConfig = ''
 					proxy_ssl_server_name on;
-					
-					allow 10.5.5.0/24; # cornwall lan
-					allow 172.16.0.0/24; # vpn
-					allow 10.3.3.2/32; # Lindsey's phone on public wifi
-					deny all;
 				'';
 			};
 			
