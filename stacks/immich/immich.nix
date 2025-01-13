1ff4496cdc7b60d274f2924feb64765b630465b9
Dan Saul <dan@dsaul.ca>
2025-01-12 22:36:36 -0600

    .

--- stacks/immich/immich.nix
+++ stacks/immich/immich.nix
@@ -27,7 +27,7 @@ services:
   immich-server:
     container_name: immich_server
     user: "${UID}:${GID}"
-    image: ghcr.io/immich-app/immich-server:v1.123.0
+    image: ghcr.io/immich-app/immich-server:v1.124.0
     volumes:
       - ${stacksDataRoot}/${packageName}/data-library:/usr/src/app/upload
     ports:
