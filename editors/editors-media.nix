4a66f839e36fa3e951ba256ba4f82715298adfde
Dan Saul <dan@dsaul.ca>
2025-01-18 11:34:01 -0600

    .

--- editors/editors-media.nix
+++ editors/editors-media.nix
@@ -1,6 +1,8 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
+	boot.kernelModules = [ "sg" ]; # for makemkv to find bluray drives
+	
 	environment.systemPackages = with pkgs; [
 		kid3-qt
 		mkvtoolnix
