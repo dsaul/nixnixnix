f8881335495018ba83e3325b6671af875a1cf050
Dan <dan@dsaul.ca>
2025-01-27 17:28:31 -0600

    .

--- generic-includes/networking-defaults-gui.nix
+++ generic-includes/networking-defaults-gui.nix
@@ -23,97 +23,97 @@ in
 			"widget.use-xdg-desktop-portal.file-picker" = 1;
 			
 			# Disable mozilla injecting preferences.
-			"app.normandy.api_url" = ""; 
-			"app.normandy.enabled" = false;
+			#"app.normandy.api_url" = ""; 
+			#"app.normandy.enabled" = false;
 			
 			# No we don't want to be your guinea pigs.
-			"app.shield.optoutstudies.enabled" = false;
-			"experiments.activeExperiment" = false;
-			"experiments.enabled" = false;
-			"experiments.manifest.uri" = "";
-			"experiments.supported" = false;
-			"network.allow-experiments" = false;
-			"network.cookie.cookieBehavior" = 1;
+			#"app.shield.optoutstudies.enabled" = false;
+			#"experiments.activeExperiment" = false;
+			#"experiments.enabled" = false;
+			#"experiments.manifest.uri" = "";
+			#"experiments.supported" = false;
+			#"network.allow-experiments" = false;
+			#"network.cookie.cookieBehavior" = 1;
 			
-			"app.update.auto" = false; # No auto updates.
-			"beacon.enabled" = false; # Don't tell websites what you click on.
-			"browser.send_pings" = false; # Don't tell websites what you click on.
-			"breakpad.reportURL" = ""; # Don't send crash reports.
-			"browser.crashReports.unsubmittedCheck.autoSubmit" = false; # Don't send crash reports.
-			"browser.crashReports.unsubmittedCheck.autoSubmit2" = false; # Don't send crash reports.
-			"browser.crashReports.unsubmittedCheck.enabled" = false; # Don't send crash reports.
+			#"app.update.auto" = false; # No auto updates.
+			#"beacon.enabled" = false; # Don't tell websites what you click on.
+			#"browser.send_pings" = false; # Don't tell websites what you click on.
+			#"breakpad.reportURL" = ""; # Don't send crash reports.
+			#"browser.crashReports.unsubmittedCheck.autoSubmit" = false; # Don't send crash reports.
+			#"browser.crashReports.unsubmittedCheck.autoSubmit2" = false; # Don't send crash reports.
+			#"browser.crashReports.unsubmittedCheck.enabled" = false; # Don't send crash reports.
 			
-			"browser.disableResetPrompt" = true; # don't prompt to reset after period of inactivity
+			#"browser.disableResetPrompt" = true; # don't prompt to reset after period of inactivity
 			
-			"browser.aboutConfig.showWarning" = false; # don't show danger warning on about:config
-			"browser.fixup.alternate.enabled" = false; # don't have firefox attempt to change uris
+			#"browser.aboutConfig.showWarning" = false; # don't show danger warning on about:config
+			#"browser.fixup.alternate.enabled" = false; # don't have firefox attempt to change uris
 			
-			"browser.newtab.preload" = false; # disable preloading of pages on new tab
-			"browser.newtabpage.activity-stream.section.highlights.includePocket" = false; # fuck pocket
-			"browser.newtabpage.enhanced" = false; # disable new tab ads
-			"browser.newtabpage.introShown" = true;  # disable new tab ads
-			"services.sync.prefs.sync.browser.newtabpage.activity-stream.showSponsoredTopSite" = false;
-			"pdfjs.enableScripting" = false;
+			#"browser.newtab.preload" = false; # disable preloading of pages on new tab
+			#"browser.newtabpage.activity-stream.section.highlights.includePocket" = false; # fuck pocket
+			#"browser.newtabpage.enhanced" = false; # disable new tab ads
+			#"browser.newtabpage.introShown" = true;  # disable new tab ads
+			#"services.sync.prefs.sync.browser.newtabpage.activity-stream.showSponsoredTopSite" = false;
+			#"pdfjs.enableScripting" = false;
 
 
 
 
 
 			# disable safebrowsing (send everything to google)
-			"browser.safebrowsing.appRepURL" = "";
-			"browser.safebrowsing.blockedURIs.enabled" = false;
-			"browser.safebrowsing.downloads.enabled" = false;
-			"browser.safebrowsing.downloads.remote.enabled" = false;
-			"browser.safebrowsing.downloads.remote.url" = "";
-			"browser.safebrowsing.enabled" = false;
-			"browser.safebrowsing.malware.enabled" = false;
-			"browser.safebrowsing.phishing.enabled" = false;
+			#"browser.safebrowsing.appRepURL" = "";
+			#"browser.safebrowsing.blockedURIs.enabled" = false;
+			#"browser.safebrowsing.downloads.enabled" = false;
+			#"browser.safebrowsing.downloads.remote.enabled" = false;
+			#"browser.safebrowsing.downloads.remote.url" = "";
+			#"browser.safebrowsing.enabled" = false;
+			#"browser.safebrowsing.malware.enabled" = false;
+			#"browser.safebrowsing.phishing.enabled" = false;
 
 			
-			"browser.search.suggest.enabled" = false; # disable sending typed form data to search provider
-			"browser.selfsupport.url" = ""; # Disable metrics to mozilla.
+			#"browser.search.suggest.enabled" = false; # disable sending typed form data to search provider
+			#"browser.selfsupport.url" = ""; # Disable metrics to mozilla.
 
-			"browser.sessionstore.privacy_level" = 0; # Allow session restore to contain form fields.
-			"browser.shell.checkDefaultBrowser" = true; # prompt if not default browser
-			"browser.startup.homepage_override.mstone" = "ignore"; # no you don't change my homepage automatically, wtf
-			"browser.tabs.crashReporting.sendReport" = false; # disable sending of crash reports
-			"browser.urlbar.groupLabels.enabled" = false; # disable firefox suggest
-			"browser.urlbar.quicksuggest.enabled" = false; # disable firefox suggest
-			"browser.urlbar.speculativeConnect.enabled" = false; # don't connect unless we actually try to connect
-			"browser.urlbar.trimURLs" = false; # don't hide part of the url bar, wtf
+			#"browser.sessionstore.privacy_level" = 0; # Allow session restore to contain form fields.
+			#"browser.shell.checkDefaultBrowser" = true; # prompt if not default browser
+			#"browser.startup.homepage_override.mstone" = "ignore"; # no you don't change my homepage automatically, wtf
+			#"browser.tabs.crashReporting.sendReport" = false; # disable sending of crash reports
+			#"browser.urlbar.groupLabels.enabled" = false; # disable firefox suggest
+			#"browser.urlbar.quicksuggest.enabled" = false; # disable firefox suggest
+			#"browser.urlbar.speculativeConnect.enabled" = false; # don't connect unless we actually try to connect
+			#"browser.urlbar.trimURLs" = false; # don't hide part of the url bar, wtf
 			#"keyword.enabled" = false;
-			"network.IDN_show_punycode" = false;
+			#"network.IDN_show_punycode" = false;
 			
 			# disable telemetry
-			"toolkit.telemetry.archive.enabled" = false;
-			"toolkit.telemetry.bhrPing.enabled" = false;
-			"toolkit.telemetry.cachedClientID" = "";
-			"toolkit.telemetry.enabled" = false;
-			"toolkit.telemetry.firstShutdownPing.enabled" = false;
-			"toolkit.telemetry.hybridContent.enabled" = false;
-			"toolkit.telemetry.newProfilePing.enabled" = false;
-			"toolkit.telemetry.prompted" = 2;
-			"toolkit.telemetry.rejected" = true;
-			"toolkit.telemetry.reportingpolicy.firstRun" = false;
-			"toolkit.telemetry.server" = "";
-			"toolkit.telemetry.shutdownPingSender.enabled" = false;
-			"toolkit.telemetry.unified" = false;
-			"toolkit.telemetry.unifiedIsOptIn" = false;
-			"toolkit.telemetry.updatePing.enabled" = false;
-			"datareporting.healthreport.service.enabled" = false;
-			"datareporting.healthreport.uploadEnabled" = false;
-			"datareporting.policy.dataSubmissionEnabled" = false;
+			#"toolkit.telemetry.archive.enabled" = false;
+			#"toolkit.telemetry.bhrPing.enabled" = false;
+			#"toolkit.telemetry.cachedClientID" = "";
+			#"toolkit.telemetry.enabled" = false;
+			#"toolkit.telemetry.firstShutdownPing.enabled" = false;
+			#"toolkit.telemetry.hybridContent.enabled" = false;
+			#"toolkit.telemetry.newProfilePing.enabled" = false;
+			#"toolkit.telemetry.prompted" = 2;
+			#"toolkit.telemetry.rejected" = true;
+			#"toolkit.telemetry.reportingpolicy.firstRun" = false;
+			#"toolkit.telemetry.server" = "";
+			#"toolkit.telemetry.shutdownPingSender.enabled" = false;
+			#"toolkit.telemetry.unified" = false;
+			#"toolkit.telemetry.unifiedIsOptIn" = false;
+			#"toolkit.telemetry.updatePing.enabled" = false;
+			#"datareporting.healthreport.service.enabled" = false;
+			#"datareporting.healthreport.uploadEnabled" = false;
+			#"datareporting.policy.dataSubmissionEnabled" = false;
 
 			# Fingerprinting Prefs
-			"device.sensors.ambientLight.enabled" = false;
-			"device.sensors.enabled" = false;
-			"device.sensors.motion.enabled" = false;
-			"device.sensors.orientation.enabled" = false;
-			"device.sensors.proximity.enabled" = false;
-			"dom.battery.enabled" = false;
-			"dom.event.clipboardevents.enabled" = false;
-			"dom.private-attribution.submission.enabled" = false;
-			"media.video_stats.enabled" = false;
+			#"device.sensors.ambientLight.enabled" = false;
+			#"device.sensors.enabled" = false;
+			#"device.sensors.motion.enabled" = false;
+			#"device.sensors.orientation.enabled" = false;
+			#"device.sensors.proximity.enabled" = false;
+			#"dom.battery.enabled" = false;
+			#"dom.event.clipboardevents.enabled" = false;
+			#"dom.private-attribution.submission.enabled" = false;
+			#"media.video_stats.enabled" = false;
 
 			# Privacy Settings
 			#"network.http.referer.spoofSource" = true;
@@ -138,28 +138,28 @@ in
 			#"webgl.vendor-string-override" = " ";
 
 			# Extensions
-			"extensions.FirefoxMulti-AccountContainers@mozilla.whiteList" = "";
-			"extensions.TemporaryContainers@stoically.whiteList" = "";
-			"extensions.getAddons.cache.enabled" = false;
-			"extensions.getAddons.showPane" = false;
-			"extensions.greasemonkey.stats.optedin" = false;
-			"extensions.greasemonkey.stats.url" = "";
-			"extensions.pocket.enabled" = false;
-			"extensions.shield-recipe-client.api_url" = "";
-			"extensions.shield-recipe-client.enabled" = false;
-			"extensions.webservice.discoverURL" = "";
+			#"extensions.FirefoxMulti-AccountContainers@mozilla.whiteList" = "";
+			#"extensions.TemporaryContainers@stoically.whiteList" = "";
+			#"extensions.getAddons.cache.enabled" = false;
+			#"extensions.getAddons.showPane" = false;
+			#"extensions.greasemonkey.stats.optedin" = false;
+			#"extensions.greasemonkey.stats.url" = "";
+			#"extensions.pocket.enabled" = false;
+			#"extensions.shield-recipe-client.api_url" = "";
+			#"extensions.shield-recipe-client.enabled" = false;
+			#"extensions.webservice.discoverURL" = "";
 
 			# Generic User Agent
-			"general.useragent.override" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.3";
+			#"general.useragent.override" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.3";
 
 			# No autoplay
-			"media.autoplay.default" = 0;
-			"media.autoplay.enabled"  = false;
+			#"media.autoplay.default" = 0;
+			#"media.autoplay.enabled"  = false;
 
 			# No encrypted media
-			"media.eme.enabled" = false;
-			"media.gmp-widevinecdm.enabled" = false;
-			"browser.eme.ui.enabled" = false;
+			#"media.eme.enabled" = false;
+			#"media.gmp-widevinecdm.enabled" = false;
+			#"browser.eme.ui.enabled" = false;
 
 
 
