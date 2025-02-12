f68504febd6a33f9bb6b42fe9a846e7fad1172d4
Dan Saul <dan@dsaul.ca>
2025-02-12 14:39:01 -0600

    .

--- generic-includes/kde6-wayland.nix
+++ generic-includes/kde6-wayland.nix
@@ -16,6 +16,7 @@
 
 	# Enable the KDE Plasma Desktop Environment.
 	services.displayManager.sddm.enable = true;
+	services.displayManager.sddm.wayland.enable = true;
 	
 	services.desktopManager.plasma6.enable = true;
 	#programs.kdeconnect.enable = true;
