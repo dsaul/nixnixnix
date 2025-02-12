e38d211bf9e6dbd43d048da184fa94ce7f86474f
Dan <dan@dsaul.ca>
2025-02-12 17:44:32 -0600

    add netatalk

--- generic-includes/tarragon.nix
+++ generic-includes/tarragon.nix
@@ -1,8 +1,15 @@
 { config, lib, pkgs, modulesPath, ... }:
-
+let
+	unstable = import <nixos-unstable> { 
+		system = "x86_64-linux"; 
+		config.allowUnfree = true; 
+		config.allowBroken = true; 
+	};
+in 
 {
 	environment.systemPackages = with pkgs; [
 		#basiliskii
+		unstable.netatalk
 		minivmac
 		(pkgs.callPackage ../packages/basiliskii/package.nix {})
 		(pkgs.callPackage ../packages/ciderpress2/package.nix {})
