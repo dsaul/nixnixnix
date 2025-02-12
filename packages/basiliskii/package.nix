b222825bd6665315033573343d8d8f41b7472ead
Dan <dan@dsaul.ca>
2025-02-11 19:54:26 -0600

    .

--- packages/basiliskii/package.nix
+++ packages/basiliskii/package.nix
@@ -45,10 +45,13 @@ stdenv.mkDerivation (finalAttrs: {
     NO_CONFIGURE=1 ./autogen.sh
   '';
   configureFlags = [
+	"--with-gtk"
 	"--with-sdl2"
     "--enable-sdl-video"
     "--enable-sdl-audio"
     "--with-bincue"
+	"--with-libvhd"
+	"--with-vdeplug"
   ];
 
   meta = with lib; {
