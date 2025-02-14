a617af2a9e9a25a4a1ad58f55c68f858d0bf1326
Dan Saul <dan@dsaul.ca>
2025-02-14 11:39:49 -0600

    .

--- nodes/cornwall-fileserver/smb-shares.nix
+++ nodes/cornwall-fileserver/smb-shares.nix
@@ -11,10 +11,16 @@
 				"path" = "/mnt/DOCUMENTS-01/Unsorted Scans";
 			};
 			Windows = {
-				browseable = "yes";  # note: each home will be browseable; the "homes" share will not.
+				browseable = "yes";
 				"read only" = "no";
 				"guest ok" = "no";
 				"path" = "/mnt/MISC-01/Software/Windows";
+			};
+			"DOCUMENTS-01" = {
+				browseable = "yes";
+				"read only" = "no";
+				"guest ok" = "no";
+				"path" = "/mnt/DOCUMENTS-01/";
 			};
 		};
 	};
