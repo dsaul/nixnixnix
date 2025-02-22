ee421a37eeb35f8ee1ea5a18364db0044653b1b1
Dan Saul <dan@dsaul.ca>
2025-02-22 10:52:44 -0600

    .

--- packages/hfsinspect/package.nix
+++ packages/hfsinspect/package.nix
@@ -1,20 +1,7 @@
 { stdenv
 , lib
 , fetchFromGitHub
-, autoconf
-, automake
-, pkg-config
-, SDL2
-, SDL2_sound
-, SDL2_image
-, gtk2
-, mpfr
-, libICE
-, ncurses
-, readline
-, libadwaita
-, gnome-themes-extra
-, makeWrapper
+, libuuid
 }:
 stdenv.mkDerivation (finalAttrs: {
 	pname = "hfsinspect";
@@ -26,28 +13,24 @@ stdenv.mkDerivation (finalAttrs: {
 		rev = "a5d0a25b9fc433faf62f71a1895be0052e892d66";
 		hash = "";
 	};
-	sourceRoot = "${finalAttrs.src.name}";
-	patches = [ 
-		
-	];
+
+
 	nativeBuildInputs = [
 		autoconf
 		automake
 		pkg-config
 	];
 	buildInputs = [
-		SDL2
-		SDL2_sound
-		SDL2_image
-		SDL2.dev
-		gtk2
-		mpfr
-		libICE
-		ncurses
-		readline
-		libadwaita
-		makeWrapper
+		libuuid
 	];
+
+
+	installPhase = ''
+	runHook preInstall
+	mkdir -p $out/bin
+	make install PREFIX=$out
+	runHook postInstall
+	'';
 
 	
 
