66b12eb829a158fd7c0b8b14bd0aaf8801985b8b
Dan <dan@dsaul.ca>
2025-03-12 19:18:45 -0500

    .

--- packages/website-dsaul.ca/package.nix
+++ packages/website-dsaul.ca/package.nix
@@ -1,4 +1,5 @@
 { buildNpmPackage
+, fetchgit
 , lib, ... }:
 let
 #	age.secrets."id_ed25519-www.dsaul.ca-nixos_package".file = ../../secrets/id_ed25519-www.dsaul.ca-nixos_package.age;
@@ -6,7 +7,7 @@ let
 in
 buildNpmPackage {
 	name = "website-dsaul.ca";
-	src = builtins.fetchgit {
+	src = fetchgit {
 		url = "git@github.com:dsaul/www.dsaul.ca.git";
 		rev = "a3d7a33b4eb547a5b9c1f32159bf5f656e960481";
 		hash = "";
